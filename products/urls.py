b575c1b9b680d988441a33e7cdc0994d7d65c8e8
Terri Grierson <tmgrierson@googlemail.com>
2021-03-05 16:27:54 +0000

    Product admin added product view, url and template

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-
+# code from boutique abo
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_detail, name='product_detail'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
