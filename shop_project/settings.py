e58df831f37087400cd0fec1306da179b6738747
Terri Grierson <tmgrierson@googlemail.com>
2021-03-11 17:46:08 +0000

    056 deployment part 2

--- shop_project/settings.py
+++ shop_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['shop-sportwear-project.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['shop-project-sportswear.herokuapp.com', 'localhost']
 
 
 # Application definition
